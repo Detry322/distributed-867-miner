f8432dc8ba83755b0f45e8584d15201174a84797
rparvat <rajeev.parvathala@gmail.com>
2016-03-01 02:23:14 -0500

    almost done with slave

--- src/common/hashconfig.go
+++ src/common/hashconfig.go
@@ -6,7 +6,17 @@ type HashConfig struct {
 	ParentId   []byte
 	Root       []byte
 	Difficulty int64
-	Timestamp  int64
+	Timestamp  uint64
 	Version    byte
-	Targets    []int64
+	Targets    []int64 // hashes that we want to match, if in mode B
+}
+
+type CollisionA struct {
+	X, Y      int64
+	Timestamp uint64
+}
+
+type CollisionB struct {
+	X, Hash   int64
+	Timestamp uint64
 }
--- src/slave/slave.go
+++ src/slave/slave.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"common"
+	"errors"
 	"fmt"
-	"log"
+	"net"
 	"net/rpc"
 	"sync"
-	// "time"
 )
 
 const (
@@ -20,24 +20,32 @@ type Slave struct {
 	Id     int64
 	Mode   int64
 	mu     sync.Mutex
-	Master
+	Master *rpc.Client
 }
 
-func (slave *Slave) StartStepA(config common.HashConfig) {
+func (slave *Slave) StartStepA(config common.HashConfig, reply *bool) (err error) {
 	slave.mu.Lock()
+	defer slave.mu.Unlock()
+	if config.Timestamp < slave.Config.Timestamp {
+		*reply = false
+		return
+	}
 	slave.Mode = A
 	slave.Config = config
-	slave.mu.Unlock()
 	// send stuff to the miner
+	return nil
 }
 
-func (slave *Slave) NewCollisionA(x, y, hash int64) {
+func (slave *Slave) NewCollisionA(x, y int64) (err error) {
 	// send rpc to master saying there's a new collision
+	collision := common.CollisionB{X: x, Y: y, Timestamp: slave.Config.Timestamp}
+	// send collision to sidd.
 }
 
 func (slave *Slave) NewCollisionB(x, hash int64) {
 	// send rpc to master saying theres a new b collision
-
+	collision := common.CollisionB{X: x, Hash: hash, Timestamp: slave.Config.Timestamp}
+	// send collision to sidd.
 }
 
 /*
@@ -47,67 +55,91 @@ func (slave *Slave) StartStepB(config common.HashConfig, reply *bool) (err error
 	slave.mu.Lock()
 	defer slave.mu.Unlock()
 	slave.Config = config
+	if config.Timestamp < slave.Config.Timestamp {
+		*reply = false
+		return
+	}
+	slave.Config = config
+	slave.Mode = B
 	// TODO: send message to miner starting step b
+	return nil
 }
 
-func getIPAddress() string {
+func getIPAddress() (s string, err error) {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		log.Fatalln(err)
+		return "", err
 	}
-
 	for _, a := range addrs {
 		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+				fmt.Println(ipnet.IP.String())
+				return ipnet.IP.String(), nil
 			}
 		}
 	}
+	return "", errors.New("no ip found")
 }
 
-func listenToMiner() {
+func (slave *Slave) listenToMiner() {
 	for {
 		//listen to messages.
 	}
 }
 
-func initiateConnection() {
+func initiateConnection(slave *Slave) {
 	client, err := rpc.Dial("tcp", address)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("worked")
-	i := 0
-	for {
-		err = client.Call("Master.Connect", i, &Slave{})
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		i++
+	slave.Master = client
+	fmt.Println("established connection")
+	ip, err := getIPAddress()
+	if err != nil {
+		// ip addr is fucked.
+
 	}
-	// rpc.Dial(network, address)
+	reply := false
+	err = slave.Master.Call("Master.Connect", ip, &reply)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	slave.listenToMiner()
 }
 
 /*
 Runs on startup
 */
 func main() {
-	initiateConnection()
+	slave := &Slave{}
+	go listenForMaster(slave)
+	initiateConnection(slave)
+}
+
+func listenForMaster(slave *Slave) {
+	// log.Debug("Listening for slaves")
+	l, e := net.Listen("tcp", ":1337")
+	if e != nil {
+		fmt.Println("fuck!")
+		// log.WithFields(log.Fields{"error": e}).Fatal("Listen error")
+	}
+	rpc.Accept(l)
 }
 
 /*
 RPC method called when master wants to ensure connection is still live.
 */
-func (slave *Slave) Heartbeat(args int, reply *int) (err error) {
+func (slave *Slave) Heartbeat(args int, reply *int64) (err error) {
 	*reply = slave.GetCount()
+	return nil
 }
 
 /*
 This sends a message to the Miner requesting a count for number of hashes computed.
 */
-func (slave *Slave) GetCount() int {
-	//
+func (slave *Slave) GetCount() int64 {
+	// TODO: should send message.
 	return 0
 }
