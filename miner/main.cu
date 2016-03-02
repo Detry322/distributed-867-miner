009a8ec3d3b9d8855487b2c61d9f50f64d0efe02
Jack Serrino <Jserrino@gmail.com>
2016-03-02 04:45:03 -0500

    this shit compiles

--- miner/main.cu
+++ miner/main.cu
@@ -3,6 +3,9 @@
 #include <helper_string.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <thread>
 
 #include "./structs.h"
 #include "./step_a.h"
@@ -12,9 +15,15 @@
 
 using namespace std;
 
-// static bool initialized = false;
-// static bool in_step_A = true;
-// static bool switched = false;
+typedef enum {
+  STATE_DOING_NOTHING,
+  STATE_WORKING_A,
+  STATE_WORKING_B
+} work_state;
+
+sha_base* global_base = NULL;
+triple* part_b_triples = NULL;
+work_state state = STATE_DOING_NOTHING;
 
 void initializeCuda() {
   cout << "=== Initializing CUDA..." << endl;
@@ -40,6 +49,13 @@ void initializeCuda() {
 }
 
 void run_step_a(sha_base* host_input) {
+  if (host_input == NULL) {
+    cout << "= Error: Nothing to copy, step B. Will definitely segfault." << endl;
+  }
+  if (part_b_triples != NULL) {
+    free(part_b_triples);
+    part_b_triples = NULL;
+  }
   uint64_t base_nonce = GET_RAND();
 
   sha_base* input;
@@ -58,6 +74,9 @@ void run_step_a(sha_base* host_input) {
 #define MEMORY_SIZE (sizeof(triple)*STEP_B_THREADS*STEP_B_BLOCKS)
 
 void run_step_b(sha_base* host_input, triple* host_triples) {
+  if (host_input == NULL || host_triples == NULL) {
+    cout << "= Error: Nothing to copy, step B. Will definitely segfault." << endl;
+  }
   sha_base* input;
   cudaMalloc(&input, sizeof(sha_base));
   cudaMemcpy(input, host_input, sizeof(sha_base), cudaMemcpyHostToDevice);
@@ -80,15 +99,118 @@ sha_base* create_base(uint8_t parentid[32], uint8_t root[32], uint64_t difficult
   return base;
 }
 
+uint64_t parse_uint64(const char* input) {
+  char buf[17];
+  strncpy(buf, input, 16); // 16 hex bytes
+  buf[16] = 0;
+  return strtoul(buf, NULL, 16); // Base 16
+}
+
+int32_t parse_int32(const char* input) {
+  char buf[9];
+  strncpy(buf, input, 8);
+  buf[8] = 0;
+  return (int32_t) strtol(buf, NULL, 16);
+}
+
+void parse_byte_array(const char* str, uint8_t result[32]) {
+  for (int i = 0; i < 32; i++) {
+    sscanf(str, "%02hhx", &result[i]);
+    str += 2;
+  }
+}
+
+void handle_update_base(const char* input) {
+  input += 2;
+  uint8_t parentid[32];
+  uint8_t root[32];
+  uint64_t difficulty;
+  uint64_t timestamp;
+  uint8_t version;
+
+  parse_byte_array(input, parentid);
+  input += 65;
+  parse_byte_array(input, root);
+  input += 65;
+  difficulty = parse_uint64(input);
+  input += 17;
+  timestamp = parse_uint64(input);
+  input += 17;
+  sscanf(input, "%02hhx", &version);
+
+  if (global_base != NULL) {
+    free(global_base);
+  }
+
+  global_base = create_base(parentid, root, difficulty, timestamp, version);
+  cout << "= Updating base..." << endl;
+};
+
+void handle_start_a(const char* input) {
+  if (global_base == NULL) {
+    cout << "= Error: Starting A but haven't received base. Will prolly segfault" << endl;
+  }
+  state = STATE_WORKING_A;
+  cout << "= Starting A..." << endl;
+};
+
+void handle_start_b(const char* input) {
+  if (global_base == NULL) {
+    cout << "= Error: Starting B but haven't received base. Will prolly segfault" << endl;
+  }
+  input += 2;
+  part_b_triples = (triple*) malloc(sizeof(triple)*STEP_B_BLOCKS*STEP_B_THREADS);
+  for (int i = 0; i < STEP_B_BLOCKS*STEP_B_THREADS; i++) {
+    for (int j = 0; j < 3; j++) {
+      part_b_triples[i].chains[j].start = parse_uint64(input);
+      input += 17;
+      part_b_triples[i].chains[j].length = parse_int32(input);
+      input += 9;
+    }
+  }
+  state = STATE_WORKING_B;
+  cout << "= Starting B..." << endl;
+};
+
 int main(int argc, char **argv) {
   srand(time(NULL));
-  initializeCuda();
-  uint8_t parentid[32] = {47, 254, 149, 3, 14, 129, 220, 200, 18, 210, 158, 85, 188, 8, 177, 74, 156, 33, 244, 140, 177, 197, 230, 199, 62, 65, 169, 208, 25, 70, 106, 26};
-  uint8_t root[32] = {21, 98, 32, 101, 67, 218, 118, 65, 35, 194, 27, 213, 36, 103, 79, 10, 138, 175, 73, 200, 168, 151, 68, 201, 115, 82, 254, 103, 127, 126, 64, 6};
-  uint64_t difficulty = 42;
-  uint64_t timestamp = 1456441489431952896L;
-  uint8_t version = 0;
-  sha_base* base = create_base(parentid, root, difficulty, timestamp, version);
-  run_step_a(base);
-  free(base);
+  //initializeCuda();
+  #pragma GCC diagnostic ignored "-Wgnu-designator"
+  struct pollfd stdin_poll = { .fd = STDIN_FILENO, .events = POLLIN | POLLRDBAND | POLLRDNORM | POLLPRI };
+  while (true) {
+    string input;
+    if (poll(&stdin_poll, 1, 0) == 1) {
+      getline(cin, input);
+      const char* cstr = input.c_str();
+      if (cstr[0] == 'H') {
+        handle_update_base(cstr);
+      } else if (cstr[0] == 'A') {
+        handle_start_a(cstr);
+      } else if (cstr[0] == 'B') {
+        handle_start_b(cstr);
+      } else {
+        cout << "= Error: Received a line of garbage input" << endl;
+      }
+    } else {
+      if (state == STATE_DOING_NOTHING) {
+        this_thread::yield();
+      } else if (state == STATE_WORKING_A) {
+        run_step_a(global_base);
+      } else if (state == STATE_WORKING_B) {
+        run_step_b(global_base, part_b_triples);
+        state = STATE_WORKING_A;
+      }
+    }
+  }
 };
+
+
+
+// uint8_t parentid[32] = {47, 254, 149, 3, 14, 129, 220, 200, 18, 210, 158, 85, 188, 8, 177, 74, 156, 33, 244, 140, 177, 197, 230, 199, 62, 65, 169, 208, 25, 70, 106, 26};
+// uint8_t root[32] = {21, 98, 32, 101, 67, 218, 118, 65, 35, 194, 27, 213, 36, 103, 79, 10, 138, 175, 73, 200, 168, 151, 68, 201, 115, 82, 254, 103, 127, 126, 64, 6};
+// uint64_t difficulty = 42;
+// uint64_t timestamp = 1456441489431952896L;
+// uint8_t version = 0;
+// sha_base* base = create_base(parentid, root, difficulty, timestamp, version);
+// run_step_a(base);
+// free(base);
