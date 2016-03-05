ae10ec640083b04a5a56bb24e660269f24939ca4
Jack Serrino <Jserrino@gmail.com>
2016-03-05 14:09:05 -0500

    piece of shit

--- miner/step_b.cu
+++ miner/step_b.cu
@@ -33,10 +33,10 @@ __global__ void step_b_kernel(sha_base* input, triple* triples) {
   uint64_t thread_id = (blockIdx.x*blockDim.x+threadIdx.x);
   uint64_t chain1 = triples[thread_id].chains[0].start;
   int32_t chain1_length = triples[thread_id].chains[0].length;
-  uint64_t chain2 = triples[thread_id].chains[0].start;
-  int32_t chain2_length = triples[thread_id].chains[0].length;
-  uint64_t chain3 = triples[thread_id].chains[0].start;
-  int32_t chain3_length = triples[thread_id].chains[0].length;
+  uint64_t chain2 = triples[thread_id].chains[1].start;
+  int32_t chain2_length = triples[thread_id].chains[1].length;
+  uint64_t chain3 = triples[thread_id].chains[2].start;
+  int32_t chain3_length = triples[thread_id].chains[2].length;
   while (true) {
     if (chain1_length > chain2_length) {
       chain1 = calculate_sha_b(chain1);
@@ -44,7 +44,6 @@ __global__ void step_b_kernel(sha_base* input, triple* triples) {
       continue;
     }
     if (chain1 == chain2) {
-      printf("= Not found early \n");
       return;
     }
     // If it gets here, chain1.length == chain2.length
@@ -58,7 +57,6 @@ __global__ void step_b_kernel(sha_base* input, triple* triples) {
     break;
   };
   if (chain1 == chain2 || chain1 == chain3 || chain2 == chain3) {
-    printf("= Not found before second loop\n");
     return;
   }
   // checking for collisions;
@@ -71,13 +69,10 @@ __global__ void step_b_kernel(sha_base* input, triple* triples) {
       printf("B %lu %lu %lu %lu\n", bbase.timestamp, chain1, chain2, chain3);
       return;
     } else if (t1 == t2) {
-      printf("= Not found, 1 and 2 collided\n");
       return;
     } else if (t2 == t3) {
-      printf("= Not found, 2 and 3 collided\n");
       return;
     } else if (t1 == t3) {
-      printf("= Not found, 1 and 3 collided\n");
       return;
     }
     chain1 = t1;
