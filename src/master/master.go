f16c749e678c9af46972724c31140629cb45e744
Siddharth Seethepalli <sidds@mit.edu>
2016-03-05 20:55:07 -0500

    sidd

--- src/master/master.go
+++ src/master/master.go
@@ -19,13 +19,13 @@ import "fmt"
 const NANOS_PER_MINUTE = 1000 * 1000 * 1000 * 60
 const NODE_URL = "http://6857coin.csail.mit.edu:8080"
 const GENESIS_HASH = "169740d5c4711f3cbbde6b9bfbbe8b3d236879d849d1c137660fce9e7884cae7"
-const BLOCK_TEXT = "Rolled my own crypto!!!1!!one!!"
+const BLOCK_TEXT = "kevintu-rahunt-sidds"
 const SLEEP_TIME_BETWEEN_SERVER_CALLS_IN_MILLIS = 15000
 const SLEEP_TIME_SHORT_IN_MILLIS = 100
 const TIMESTAMP_WINDOW_IN_MINUTES = 8
 const SEND_THRESHOLD = 8192
 const MINE_ON_GENESIS = false
-const MINE_HARDCODED = false
+const MINE_HARDCODED = true
 const OVERRIDE_DIFFICULTY = 50
 
 func init() {
@@ -183,7 +183,7 @@ func commitBlock(master *Master, b common.Block, text string) {
 	fmt.Println("3", b.Nonces[2])
 	fmt.Println("ver", b.Version)
 	fmt.Println(text)
-	log.Error("Attempting to commit block!!!!!!!")
+	log.Fatal("Attempting to commit block!!!!!!!")
 	encoded := fmt.Sprintf(`
 {
   "header": {
@@ -209,7 +209,7 @@ func commitBlock(master *Master, b common.Block, text string) {
 	}
 	resp.Body.Close()
 	log.Warn(resp.Status)
-	log.Fatal("Attempted to commit block!!!!!!!")
+	log.Error("Attempted to commit block!!!!!!!")
 }
 
 func (m *Master) Connect(ip string, reply *bool) (err error) {
